45a9ab6cac7ffd8b29a09d049deed1fef0a05863
Petr Valenta <petr.valenta.92@gmail.com>
2024-02-13 21:40:05 +0100

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,11 +2,24 @@ export default function About() {
     return (
         <div className="about">
             <h3>About</h3>
-            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
-                Integer imperdiet lectus quis justo. In rutrum. Curabitur ligula sapien, 
-                pulvinar a vestibulum quis, facilisis vel sapien. 
-                Duis pulvinar. 
+            <p>
+                I am a 30+ year old man with the soul of a child who wants to constantly explore, 
+                try and learn something new. When I learn something, I try to learn it properly 
+                and correctly. I am not afraid of mistakes and if I ever make one, I learn from 
+                it and am wiser and better because of it. 
             </p>
         </div>
     )
 }
+
+/*
+Hello and welcome! 
+You may be asking, "Who is Peter?" Let me briefly share my story with you. At the age of 28, I realized that I wanted to be a part of something that was meaningful, that would help people live a simpler life, and that would be dynamic and bring me joy of the ride. So I decided to change my career, work on myself every day, learn to code and become a developer. After some time of searching, trying and failing, I found my style and completely focused on front-end development (HTML, CSS, JS, React.js, TypeScript etc).
+I am currently a PRO student of @Scrimba. Here I'm going through a complete course for future front-end developers. 
+I would like to find an entry-level job as a front-end developer where I could learn new technologies, methods, skills and diverse approaches to development. 
+I know the road is long and challenging, but I am fully committed to making my dreams come true!
+
+You can check some of my work on my GitHub (https://github.com/PetrValenta92)
+
+Feel free to contact me directly at petr.valenta.92@gmail.com or at LinkedIn
+*/
