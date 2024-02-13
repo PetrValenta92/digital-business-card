cc961f84d633c2555f1afddca7a5130a7413f8be
Petr Valenta <petr.valenta.92@gmail.com>
2024-02-13 20:21:39 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,13 @@
-import twitter_icon from '../images/twitter_icon.svg';
-import instagram_icon from '../images/instagram_icon.svg';
-import github_icon from '../images/github_icon.svg';
+import twitterIcon from '../images/twitter.svg';
+import instagramIcon from '../images/instagram.svg';
+import githubIcon from '../images/github.svg';
 
 export default function Footer() {
     return (
         <footer className="footer">
-            <a href="https://twitter.com/PedroValenta" target="_blank"><img src={ twitter_icon } alt="Twitter icon" /></a>
-            <a href="https://www.instagram.com/petr_thedev/" target="_blank"><img src={ instagram_icon } alt="Instagram icon" /></a>
-            <a href="https://github.com/PetrValenta92" target="_blank"><img src={ github_icon } alt="GitHub icon" /></a>
+            <a href="https://twitter.com/PedroValenta" target="_blank"><img src={ twitterIcon } alt="Twitter icon" /></a>
+            <a href="https://www.instagram.com/petr_thedev/" target="_blank"><img src={ instagramIcon } alt="Instagram icon" /></a>
+            <a href="https://github.com/PetrValenta92" target="_blank"><img src={ githubIcon } alt="GitHub icon" /></a>
         </footer>
     )
 }
