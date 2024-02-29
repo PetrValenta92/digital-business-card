2e1018c052c4d6dedd3e44ec3f3a7e1ebaa59c9f
Petr Valenta <petr.valenta.92@gmail.com>
2024-02-29 21:26:40 +0100

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,15 +11,3 @@ export default function About() {
         </div>
     )
 }
-
-/*
-Hello and welcome! 
-You may be asking, "Who is Peter?" Let me briefly share my story with you. At the age of 28, I realized that I wanted to be a part of something that was meaningful, that would help people live a simpler life, and that would be dynamic and bring me joy of the ride. So I decided to change my career, work on myself every day, learn to code and become a developer. After some time of searching, trying and failing, I found my style and completely focused on front-end development (HTML, CSS, JS, React.js, TypeScript etc).
-I am currently a PRO student of @Scrimba. Here I'm going through a complete course for future front-end developers. 
-I would like to find an entry-level job as a front-end developer where I could learn new technologies, methods, skills and diverse approaches to development. 
-I know the road is long and challenging, but I am fully committed to making my dreams come true!
-
-You can check some of my work on my GitHub (https://github.com/PetrValenta92)
-
-Feel free to contact me directly at petr.valenta.92@gmail.com or at LinkedIn
-*/
